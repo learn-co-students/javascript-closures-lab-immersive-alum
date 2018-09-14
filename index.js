81295382b14d04be2ed7bd3cd8d2949c4428227a
yongnicholaskim <yongnicholaskim@gmail.com>
2018-09-14 14:03:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  let counter = 0
+
+  return function addBump() {
+    counter++
+  };
+
+  return function getBumps() {
+    return counter;
+  };
+}
+
+function createAnimal(animalType) {
+  return function (deadlyDevice) {
+    return { animalType, deadlyDevice };
+  }
+}
+
+let sharkCreator = createAnimal('Shark');
+let sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+let sharkWithFrickinLaserbeam = sharkCreator('Cannon');
